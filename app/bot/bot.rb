5c1e782627f14e8b2ebd301f2351c4227e7c7b8c
Andryhajanirina <andryhaj@gmail.com>
2018-07-11 16:51:07 +0300

    Modif bot.rb

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -4,5 +4,11 @@ include Facebook::Messenger
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV['ACCESS_TOKEN'])
 
 Bot.on :message do |message|
-  message.reply(text: 'Hello, human!')
+	if message.include? "Bonjour"
+  		message.reply(text: "Salut, l'amie!")
+  	elsif message.include? "Quoi de neuf ?"
+  		message.reply(text: "Rien de spécial ! et de ton coté ?")
+  	else
+  		message.reply(text: "Au revoir!")
+  	end  			
 end
